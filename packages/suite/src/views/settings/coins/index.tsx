cdbebe64bb56d46e7387e506b12375aab8b31f74
Marek Polak <marek.polak@satoshilabs.com>
2022-01-21 12:20:10 +0100

    fixup! feat(suite): coins list redesigned

--- packages/suite/src/views/settings/coins/index.tsx
+++ packages/suite/src/views/settings/coins/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SettingsLayout } from '@settings-components';
-import { CoinsGroup, Card } from '@suite-components';
+import { CoinsGroup, Translation } from '@suite-components';
+import { Section } from '@suite-components/Settings';
 import { useEnabledNetworks } from '@settings-hooks/useEnabledNetworks';
 
 const StyledSettingsLayout = styled(SettingsLayout)`
@@ -10,26 +11,30 @@ const StyledSettingsLayout = styled(SettingsLayout)`
     }
 `;
 
+const StyledCoinsGroup = styled(CoinsGroup)`
+    margin-top: 18px;
+`;
+
 const Settings = () => {
     const { mainnets, testnets, enabledNetworks, setEnabled } = useEnabledNetworks();
 
     return (
         <StyledSettingsLayout>
-            <Card>
-                <CoinsGroup
+            <Section title={<Translation id="TR_COINS" />}>
+                <StyledCoinsGroup
                     networks={mainnets}
                     onToggle={setEnabled}
                     selectedNetworks={enabledNetworks}
                 />
-            </Card>
-            <Card>
-                <CoinsGroup
+            </Section>
+            <Section title={<Translation id="TR_TESTNET_COINS" />}>
+                <StyledCoinsGroup
                     networks={testnets}
                     onToggle={setEnabled}
                     selectedNetworks={enabledNetworks}
                     testnet
                 />
-            </Card>
+            </Section>
         </StyledSettingsLayout>
     );
 };
